7c7bf9f756ecfd612cadbdff0493cf931be6474a
fluiz <fluiz.cba@gmail.com>
2018-01-17 23:03:47 -0300

    Mudanças beeeeem pequenas foram feitas, tirei as docstring por não estarem bem feitas e o modulo File parecer não existir na versão 3 da linguagem

--- user.py
+++ user.py
@@ -4,35 +4,16 @@ from selenium import webdriver
 from selenium.common import exceptions as error
 
 class User:
-    """\tRepresents a instagram user 
-    
-        This class represents a instagram user and can following or send 
-        messages to outhers users
-
-        Attributes:
-            username: Name, cell phone number or e-mail used to login
-                      on instagram
-            password: password of instagram account
-    """
     def __init__(self, username, password):
-        """\tConstructor
-
-            Args:
-                username: Name, cell phone number or e-mail used to login
-                      on instagram
-                password: password of instagram account
-        """
         self._username = username
         self._password = password
         self._driver = None
 
     def login(self):
-        """\tLogin a user account"""
         driver = webdriver.Chrome()
-        INSTAGRAM_URL = "https://www.instagram.com/accounts/login/"
 
         try:
-            driver.get(INSTAGRAM_URL)
+            driver.get('https://www.instagram.com/accounts/login/')
             driver.find_element_by_name("username").clear()
             driver.find_element_by_name("username").send_keys(self._username)
             driver.find_element_by_name("password").clear()
@@ -49,6 +30,12 @@ class User:
             for i, url_follower in enumerate(urls_followers):
                 print(str(i), '-', url_follower)
                 self._driver.get(url_follower)
-                time.sleep(7)
-                self._driver.find_element_by_class_name('_qv64e').click()
-                time.sleep(7)
+
+                try:
+                    if self._driver.find_element_by_class_name('_qv64e').text == 'Seguir':
+                        time.sleep(random.randint(7, 10))
+                        self._driver.find_element_by_class_name('_qv64e').click()
+                        time.sleep(random.randint(7, 10))
+                except error.WebDriverException:
+                    print("<<<ERRO>>>", url_follower, "<<<ERRO>>>")
+        
